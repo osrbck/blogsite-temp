fd91b2dcce7ca00fbeb9fcd2b78409729a9d7eda
Osman <osrbck@gmail.com>
2024-07-25 14:05:09 +0300

    project created

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { Navbar } from "./components"
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+import React, {useState} from 'react';
+import {logo, menu, close} from "../assets";
+
+const Navbar = () => {
+
+    const [toggle, setToogle]=useState(true);
+    const handleClick = () => setToogle(!toggle);
+
+  return (
+    <div className='w-screen h-[80px] z-10 bg-white fixed drop-shadow-lg'>
+        <div className='flex justify-between items-center w-full h-full md:max-w-[1240] m-auto'>
+            <div className='flex items-center'>
+                <img src={logo} alt="logo" className="ml-10 opacity-[75%] w-full h-[80px]"/>
+            </div>
+
+            <div className='flex items-center'>
+                <ul className='hidden sm:flex'>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Support</li>
+                    <li>Platform</li>
+                </ul>
+            </div>
+
+            <div className='hidden sm:flex mr-10'>
+                <button className='px-8 py-3 md:mr-10'>Log In</button>
+            </div>
+
+            <div className='sm:hidden' onClick={handleClick}>
+                <img src={toggle?menu:close} alt='menu' className='w-[33px] h-[33px] object-contain mr-10' />
+            </div>
+        </div>
+        
+        {/* toggled nav */}
+        <ul className={!toggle?'absolute bg-white w-full text-center px-8 sm:hidden':'hidden'}>
+                <li>Home</li>
+                <li>About</li>
+                <li>Support</li>
+                <li>Platform</li>
+                <div className='flex flex-col my-4'>
+                    <button className='px-8 py-3 md:mr-10'>Log In</button>
+                </div>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar;
